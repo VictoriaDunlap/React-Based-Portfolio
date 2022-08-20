a1ae732b91128f134112da5a440207c49bf53f55
victoriaDunlap <victoriacdunlap@gmail.com>
2022-08-20 13:57:12 -0400

    AboutMe description added

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,17 +1,21 @@
 import React from 'react'
 
+const style = {
+    width: "200px;"
+}
+
 function AboutMe() {
     return(
     <figure>
     <blockquote class="blockquote">
-    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-  
+    <p class="mx-5 my-5" style={style}>Full stack web developer leveraging a degree in PR and Marketing to create beautiful and interactive layouts to reach and engage large audiences. Completed a certificate from the Georgia Institute of Technology in full-stack development. Through this program I have harnessed skills in HTML, CSS, GIT, JavaScript, Node.js, React.js, Express.js, SQL, and MERN Stack. Experienced team player and problem solver with a focus on mobile-first design, ready to chat about what can be done for you!</p>
     </blockquote>
-    <figcaption class="blockquote-footer">
-    Someone famous in <cite title="Source Title">Source Title</cite>
-    </figcaption>
     </figure>
     )
 }
+
+{/* <figcaption class="blockquote-footer">
+Someone famous in <cite title="Source Title">Source Title</cite>
+</figcaption> */}
 
 export default AboutMe
