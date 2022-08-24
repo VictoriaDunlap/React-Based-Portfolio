f31bd35097b075330394c9b06e07d51b7040277b
victoriaDunlap <victoriacdunlap@gmail.com>
2022-08-23 20:51:15 -0400

    email added to the footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -10,11 +10,16 @@ function Footer() {
     return(
 
         <footer class="text-center text-white">
-       
+          <h1 class="mt-5">Lets Keep in Touch</h1>
           <div class="container pt-4">
             
             <section class="mb-4">
-         
+                
+                <a class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://accounts.google.com/signin/v2/identifier?flowName=GlifWebSignIn&flowEntry=ServiceLogin" role="button">
+                    <i class="bi bi-envelope-open-heart-fill"> victoriacdunlap@gmail.com</i>
+                </a>
+
+
                 <a class="btn btn-primary btn-floating m-1" target="_blank" rel="noreferrer noopener" href="https://github.com/VictoriaDunlap" role="button">
                     <i class="bi bi-github"> GitHub</i>
                 </a>
